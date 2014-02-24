de01c37792f68fdc1f538277b2f9e9825edef883
Joseph Lisee <jlisee@gmail.com>
2014-02-23 22:10:17 -0500

    Broke off general utility funtions into util.go file and util_test.go
    This makes it more clear where the real code for the project lies vs.
    helper software.

--- core_test.go
+++ core_test.go
@@ -80,68 +80,6 @@ func TestParseArgs(t *testing.T) {
 	}
 }
 
-func TestTempFile(t *testing.T) {
-	f, err := TempFile("", "cbd-test-", ".test")
-
-	if err != nil {
-		t.Errorf("Error:", err)
-	}
-
-	name := f.Name()
-
-	defer os.Remove(name)
-
-	// Now lets check the file
-	prefix := filepath.Join(os.TempDir(), "cbd-test-")
-	suffix := ".test"
-
-	if !strings.HasPrefix(name, prefix) {
-		t.Errorf("Error '%s' does not have prefix: '%s'", name, prefix)
-	}
-
-	if !strings.HasSuffix(name, suffix) {
-		t.Errorf("Error '%s' does not have suffix: '%s'", name, suffix)
-	}
-}
-
-// Put in a test for RunCmd here, make sure we are getting back stderr and stdout
-func TestRunCmd(t *testing.T) {
-	tests := map[string]ExecResult{
-		"go version": ExecResult{
-			Output: []byte("go version go1.2 linux/amd64\n"),
-			Return: 0,
-		},
-		"go bob": ExecResult{
-			Output: []byte("go: unknown subcommand \"bob\"\nRun 'go help' for usage.\n"),
-			Return: 2,
-		},
-	}
-
-	for cmd, eres := range tests {
-		// Split up string to get the command and it's args
-		args := strings.Split(cmd, " ")
-
-		res, err := RunCmd(args[0], args[1:])
-
-		// Ignore the errors that occur with non-zero return codes
-		if eres.Return == 0 {
-			if err != nil {
-				t.Errorf("Run command failed with: %s", err)
-			}
-		}
-
-		// Now check our results
-		if res.Return != eres.Return {
-			t.Errorf("Got return: %d instead of: %d", eres.Return, res.Return)
-		}
-
-		if 0 != bytes.Compare(res.Output, eres.Output) {
-			t.Errorf("Got output: %s instead of: %s", string(res.Output),
-				string(eres.Output))
-		}
-	}
-}
-
 // This test requires gcc to be installed
 func TestPreprocess(t *testing.T) {
 	b := ParseArgs(strings.Split("-c data/main.c -o main.o", " "))
---
+++ util.go
@@ -0,0 +1,124 @@
+// This package contains utility functions that are miscellaneous or fill in
+// gaps in Go's standard library.  Either way that are not specific to this
+// project.
+//
+// Author: Joseph Lisee <jlisee@gmail.com>
+
+package cbuildd
+
+import (
+	"bytes"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"math/big"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"syscall"
+)
+
+// The result of running a command
+type ExecResult struct {
+	Output []byte // Output of the command
+	Return int    // Return code of program
+}
+
+// Executes, returning the stdout if the program fails (the return code is
+// ignored)
+func RunCmd(prog string, args []string) (result ExecResult, err error) {
+	fmt.Printf("Run: %s ", prog)
+	for _, arg := range args {
+		fmt.Printf("%s ", arg)
+	}
+	fmt.Println()
+
+	cmd := exec.Command(prog, args...)
+
+	// Setup the buffer to hold the output
+	// TODO: consider caching this buffer
+	buffer := new(bytes.Buffer)
+
+	cmd.Stdout = buffer
+	cmd.Stderr = buffer
+
+	err = cmd.Run()
+
+	// Copy over our buffer
+	result.Output = buffer.Bytes()
+
+	// Get the return code out of the error
+	if err != nil {
+		result.Return = -1
+
+		// Possibly Linux specific example
+		if exiterr, ok := err.(*exec.ExitError); ok {
+			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+				result.Return = status.ExitStatus()
+			}
+		}
+	}
+
+	return
+}
+
+// copies dst to src location, no metadata is copied
+func Copyfile(dst, src string) error {
+	s, err := os.Open(src)
+
+	if err != nil {
+		return err
+	}
+
+	// No need to check errors on read only file, we already got everything
+	// we need from the filesystem, so nothing can go wrong now.
+	defer s.Close()
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+	return d.Close()
+}
+
+// Generates and opens a temporary file with a defined prefix and suffix
+// This is the same api as ioutil.TempFile accept it accepts a suffix
+//  TODO: see if this is too slow
+func TempFile(dir, prefix string, suffix string) (f *os.File, err error) {
+	if dir == "" {
+		dir = os.TempDir()
+	}
+
+	// The maximum size of random file count
+	// TODO: see if we can do this at package scope somehow
+	var maxRand *big.Int = big.NewInt(0)
+	maxRand.SetString("FFFFFFFFFFFFFFFF", 16)
+
+	var randNum *big.Int
+
+	for i := 0; i < 10000; i++ {
+		// Generate random part of the path name
+		randNum, err = rand.Int(rand.Reader, maxRand)
+
+		if err != nil {
+			return
+		}
+
+		// Transform to an int
+		randString := hex.EncodeToString(randNum.Bytes())
+
+		// Attempt to open file and fail if it already exists
+		name := filepath.Join(dir, prefix+randString+suffix)
+		f, err = os.OpenFile(name, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0600)
+		if os.IsExist(err) {
+			continue
+		}
+		break
+	}
+	return
+}
