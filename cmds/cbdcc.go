3b9d70768adad6eeba28acb6c026ee20f531b07a
Joseph Lisee <jlisee@gmail.com>
2014-03-01 22:58:59 -0500

    cbdcc now will now use the server if CBD_SERVER is set to find workers
    The code will prioritize CBD_POTENTIAL_HOST over CBD_SERVER if present. If
    the remote build fails for some reason a local build is run instead.
    
    The test.sh bash script has also been updated to check the output of the
    test progam and to test the server version of the compile build.

--- cmds/cbdcc.go
+++ cmds/cbdcc.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -55,17 +56,7 @@ func main() {
 		}
 
 		// See if we have a remote host defined
-		host := os.Getenv("CBD_POTENTIAL_HOST")
-
-		var cresults cbd.CompileResult
-
-		if len(host) > 0 {
-			// Build it remotely
-			cresults, err = buildRemote(host, job)
-		} else {
-			// Build it locally
-			cresults, err = job.Compile()
-		}
+		cresults, err := buildJob(job)
 
 		if err != nil || cresults.Return != 0 {
 			fmt.Print(string(cresults.Output))
@@ -97,12 +88,80 @@ func main() {
 	}
 }
 
+func buildJob(job cbd.CompileJob) (cresults cbd.CompileResult, err error) {
+	address := os.Getenv("CBD_POTENTIAL_HOST")
+	server := os.Getenv("CBD_SERVER")
+	local := false
+
+	// If we have a server, but no hosts, go with the server
+	if len(address) == 0 && len(server) > 0 {
+		address, err = findWorker(server)
+
+		if err != nil {
+			log.Print("Find worker error: ", err)
+		}
+	}
+
+	// Try to build on the remote host if we have found one
+	if len(address) > 0 {
+		cresults, err = buildRemote(address, job)
+
+		// If the remote build failed switch to local
+		if err != nil {
+			log.Print("Remote build error: ", err)
+			local = true
+		}
+	} else {
+		local = true
+	}
+
+	// Build it locally if all else has failed
+	if local {
+		cresults, err = job.Compile()
+	}
+
+	return cresults, err
+}
+
+// findWorker uses a central server to find the desired worker
+func findWorker(address string) (worker string, err error) {
+	// Connect to server
+	mc, err := cbd.NewTCPMessageConn(address, time.Duration(10)*time.Second)
+
+	// Get hostname
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		return
+	}
+
+	// Send our request
+	rq := cbd.WorkerRequest{
+		Client: hostname,
+	}
+	mc.Send(rq)
+
+	// Read back our response
+	r, err := mc.ReadWorkerResponse()
+
+	if err != nil {
+		return
+	}
+
+	worker = r.Worker + ":" + strconv.Itoa(r.Port)
+	return worker, nil
+}
+
 // Build the given job on the remote host
 func buildRemote(address string, job cbd.CompileJob) (cbd.CompileResult, error) {
 	var result cbd.CompileResult
 
 	// Connect to the remote host so we can have it build our file
 	mc, err := cbd.NewTCPMessageConn(address, time.Duration(10)*time.Second)
+
+	if err != nil {
+		return result, err
+	}
 
 	// Send the build job
 	mc.Send(job)
--- test.sh
+++ test.sh
@@ -9,37 +9,50 @@ function clean() {
 }
 
 function checkout() {
+    echo "[Running: ./test-main]"
     testout=$(./test-main)
 
     if [ "$testout" != "Hello, world!" ]; then
         echo "Output Invalid got value '$testout'"
         exit 1
+    else
+        echo "  GOOD"
     fi
+}
+
+function disp() {
+    echo
+    echo $*
 }
 
 clean
 
 # Run tests
+disp "[Running tests]"
 go test
 
 # Build everything
+disp "[Build and install]"
 go install
 go build cmds/cbdcc.go
 go build cmds/cbd.go
 mv cbdcc cbd $GOPATH/bin
 
 # The compile the program
+disp "[Local only test]"
+
 export CBD_POTENTIAL_HOST=''
 
 cbdcc gcc -c data/main.c -o main.o
 cbdcc gcc main.o -o test-main
 checkout # Test the output
 
-
 # Clean up
 clean
 
 # Now lets do it again over the network
+disp "[Direct worker test]"
+
 cbd &
 d_pid=$!
 trap "kill -9 ${d_pid}" EXIT
@@ -51,5 +64,26 @@ cbdcc gcc main.o -o test-main
 checkout # Test the output
 
 clean
+kill -9 ${d_pid} &> /dev/null
 
-# Now lets do again over with a server
+# Now lets do again over with a server and a worker
+disp "[Server & worker test]"
+
+export CBD_POTENTIAL_HOST=''
+export CBD_SERVER="localhost:15800"
+
+cbd -address $CBD_SERVER -server &
+a_pid=$!
+trap "kill -9 ${a_pid}" EXIT
+
+cbd -address ":15786" &
+d_pid=$!
+trap "kill -9 ${d_pid}" EXIT
+
+sleep 1
+
+cbdcc gcc -c data/main.c -o main.o
+cbdcc gcc main.o -o test-main
+checkout # Test the output
+
+clean
