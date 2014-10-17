31866873add85caefb73b7dd6fccac0868d766d9
Joseph Lisee <jlisee@gmail.com>
2014-10-16 21:50:01 -0400

    Move monitor functionality into package out of function
    We want to consolidate into one command, so we need to move things out of
    the commands.

--- cmds/cbdmon.go
+++ cmds/cbdmon.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/jlisee/cbd"
 	"os"
-	"reflect"
 	"time"
+
+	"github.com/jlisee/cbd"
 )
 
 func main() {
@@ -13,8 +13,9 @@ func main() {
 	// Make connection to server
 	server := os.Getenv("CBD_SERVER")
 
+	m := cbd.NewMonitor(server)
 	for {
-		mc, err := connect(server)
+		err := m.Connect()
 
 		// Keep trying until we get a connection
 		if err != nil {
@@ -24,72 +25,11 @@ func main() {
 		}
 
 		// Go into an infinite reporting loop
-		err = report(mc)
+		err = m.BasicReport()
 
 		// A clear exit from report means we should do a graceful shutdown
 		if err == nil {
 			break
-		}
-	}
-}
-
-// Connects to the the server and requests monitoring data
-func connect(address string) (*cbd.MessageConn, error) {
-	mc, err := cbd.NewTCPMessageConn(address, time.Duration(10)*time.Second)
-
-	if err != nil {
-		return nil, err
-	}
-
-	// Get hostname
-	hostname, err := os.Hostname()
-
-	if err != nil {
-		fmt.Printf("Could not get hostname: %s", err)
-		return nil, err
-	}
-
-	// Generate a unique host identifier
-	hostid := fmt.Sprintf("%s(%d)", hostname, os.Getpid())
-
-	// Send out monitor request
-	rq := cbd.MonitorRequest{
-		Host: hostid,
-	}
-	mc.Send(rq)
-
-	return mc, nil
-}
-
-// Run forever reporting, return error if something goes wrong
-func report(mc *cbd.MessageConn) error {
-	for {
-		_, i, err := mc.Read()
-
-		if err != nil {
-			// TODO: check for stale data
-			return err
-		}
-
-		switch m := i.(type) {
-		case cbd.CompletedJob:
-			fmt.Printf("%s: finished job in: %.3fs (Speed: %.0f)\n", m.Worker,
-				m.CompileTime.Seconds(), m.CompileSpeed)
-
-		case cbd.WorkerStateList:
-			// Final output
-			// id?  Preprocess/Compile    file.cpp                     server[core#]
-
-			fmt.Printf("[")
-			for _, state := range m.Workers {
-				// element is the element from someSlice for where we are
-				fmt.Printf("%s[%d|%d] ", state.Host, state.Load, state.Capacity)
-			}
-			fmt.Printf("]\n")
-
-		default:
-			fmt.Printf("ERROR, unknown message type: %s\n",
-				reflect.TypeOf(i).Name())
 		}
 	}
 }
--- monitor.go
+++ monitor.go
@@ -6,6 +6,9 @@
 package cbd
 
 import (
+	"fmt"
+	"os"
+	"reflect"
 	"time"
 )
 
@@ -22,4 +25,94 @@ type CompletedJob struct {
 // We define the compile speed of a job based
 func (c *CompletedJob) computeCompileSpeed() {
 	c.CompileSpeed = float64(c.OutputSize) / c.CompileTime.Seconds() / 1024
+}
+
+type Monitor struct {
+	saddr string       // Address for the server
+	mc    *MessageConn // Message connection to server
+}
+
+// Creates a new connects to the server
+func NewMonitor(saddr string) *Monitor {
+	m := new(Monitor)
+	m.saddr = saddr
+	m.mc = nil
+
+	return m
+}
+
+// Connect to the server and sends monitoring request
+func (m *Monitor) Connect() error {
+	var err error
+	m.mc, err = NewTCPMessageConn(m.saddr, time.Duration(10)*time.Second)
+
+	if err != nil {
+		m.mc = nil
+		return err
+	}
+
+	// Get hostname
+	hostname, err := os.Hostname()
+
+	if err != nil {
+		fmt.Printf("Could not get hostname: %s", err)
+		m.mc = nil
+		return err
+	}
+
+	// Generate a unique host identifier
+	hostid := fmt.Sprintf("%s(%d)", hostname, os.Getpid())
+
+	// Send out monitor request
+	rq := MonitorRequest{
+		Host: hostid,
+	}
+	m.mc.Send(rq)
+
+	return nil
+}
+
+// Print out report data in raw form, connecting if needed
+func (m *Monitor) BasicReport() error {
+	// Connect of needed
+	if m.mc == nil {
+		err := m.Connect()
+
+		if err != nil {
+			return err
+		}
+	}
+
+	// Jump into our reporting loop
+	for {
+		_, i, err := m.mc.Read()
+
+		if err != nil {
+			// TODO: check for stale data
+			m.mc = nil
+
+			return err
+		}
+
+		switch m := i.(type) {
+		case CompletedJob:
+			fmt.Printf("%s: finished job in: %.3fs (Speed: %.0f)\n", m.Worker,
+				m.CompileTime.Seconds(), m.CompileSpeed)
+
+		case WorkerStateList:
+			// Final output
+			// id?  Preprocess/Compile    file.cpp                     server[core#]
+
+			fmt.Printf("[")
+			for _, state := range m.Workers {
+				// element is the element from someSlice for where we are
+				fmt.Printf("%s[%d|%d] ", state.Host, state.Load, state.Capacity)
+			}
+			fmt.Printf("]\n")
+
+		default:
+			fmt.Printf("ERROR, unknown message type: %s\n",
+				reflect.TypeOf(i).Name())
+		}
+	}
 }
